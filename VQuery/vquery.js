d9c50e82ab3824e1cd15e48cf4dc60b0c499d31f
lichong <543281892@qq.com>
2018-03-22 16:11:14 +0800

    DOM ok, table ok

---
+++ VQuery/vquery.js
@@ -0,0 +1,83 @@
+function addEvent(node, event, fn) {
+    if(node.attachEvent) {
+        // IE
+        node.attachEvent('on'+event, function () {
+            // 调用函数，改变this为node
+            fn.call(node)
+        })
+
+    } else {
+        node.addEventListener(event, fn)
+    }
+}
+function getStyle(node, attr) {
+    if(node.currentStyle) {
+        return node.currentStyle[attr]
+    } else {
+        return getComputedStyle(node)[attr]
+    }
+
+}
+
+
+function VQuery(vArg) {
+    this.elements = []
+    switch (typeof vArg) {
+        case 'function':
+            addEvent(window, 'load', vArg)
+            break
+        case 'string':
+            var prefix = vArg.charAt(0)
+            if (prefix === '#') {
+                this.elements.push(document.getElementById(vArg.substring(1)))
+            } else if (prefix === '.') {
+                // IE9
+                this.elements = document.getElementsByClassName(vArg.substring(1))
+            } else {
+                this.elements = document.getElementsByTagName(vArg)
+            }
+            break
+        default:
+            this.elements.push(vArg)
+    }
+}
+
+VQuery.prototype.css = function (attr, value) {
+    if (value === undefined) {
+        return getStyle(this.elements[0], attr)
+    }
+    var length = this.elements.length
+    for (var i = 0; i < length; i++) {
+        var ele = this.elements[i]
+        ele.style[attr] = value
+
+    }
+}
+
+VQuery.prototype.click = function (fn) {
+    var length = this.elements.length
+    for (var i = 0; i < length; i++) {
+        var ele = this.elements[i]
+        addEvent(ele, 'click', fn)
+
+    }
+}
+
+VQuery.prototype.show = function () {
+    this.css('display', 'block')
+}
+
+VQuery.prototype.hide = function () {
+    this.css('display', 'none')
+}
+VQuery.prototype.hover = function (mouseEnterCb, mouseLeaveCb) {
+    for (var i = 0; i < this.elements.length; i++) {
+        var ele = this.elements[i]
+        addEvent(ele, 'mouseenter', mouseEnterCb)
+        addEvent(ele, 'mouseleave', mouseLeaveCb)
+    }
+
+}
+function $$(vArg) {
+    return new VQuery(vArg)
+}
--- utils/common.js
+++ utils/common.js
@@ -1,5 +1,19 @@
-module.exports = {
-    genLongHTML: function () {
-        
+var genLongHTML = function () {
+    var ss = ''
+    for (let i = 0; i < 1000; i++) {
+        ss += '<div>' + i + '</div>'
     }
+    // document.body.insertAdjacentHTML('afterbegin', ss)
+    var wrapper = document.createElement('div')
+    wrapper.id = 'wrapper'
+    wrapper.innerHTML = ss
+    document.body.insertBefore(wrapper, document.body.firstChild)
+
+}
+
+function getRandomInt(min, max) {
+    // return a random integer of min <= x < max
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min)
 }
