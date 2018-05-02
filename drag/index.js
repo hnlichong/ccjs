b52e0a87bd6c1d93169d4e31310e62994e535933
lichong <543281892@qq.com>
2018-05-02 10:02:43 +0800

    drag touch ok

--- drag/index.js
+++ drag/index.js
@@ -1,22 +1,44 @@
-function drag(el, doc = document, de = document.documentElement, bd = document.body) {
+'use strict'
+
+function Drag(el) {
+    this.el = el
+    let isTouchDevice = ('ontouchstart' in document)
+    this.evs = isTouchDevice? {
+        start: 'touchstart',
+        move: 'touchmove',
+        end: 'touchend'
+    }:{
+        start: 'mousedown',
+        move: 'mousemove',
+        end: 'mouseup'
+    }
+    this.startHandler = new Function()
+
+}
+Drag.prototype.enable = function () {
+    const el = this.el,
+        doc = document,
+        de = doc.documentElement,
+        bd = doc.body,
+        evs = this.evs
     // 距离不变，三个事件
-    function mousedownHandler(ev) {
+    this.startHandler = function (ev) {
         let sl = de.scrollLeft || bd.scrollLeft,
             st = de.scrollTop || bd.scrollTop,
             dw = de.clientWidth || bd.clientWidth,
             dh = de.clientHeight || bd.clientHeight,
             rect = el.getBoundingClientRect(),
-            dX = sl + ev.clientX - rect.left,
-            dY = st + ev.clientY - rect.top,
+            dX = sl + (ev.clientX || ev.touches[0].clientX) - rect.left,
+            dY = st + (ev.clientY || ev.touches[0].clientY) - rect.top,
             maxLeft = sl + dw - rect.width,
             maxTop = st + dh - rect.height
-        doc.addEventListener('mousemove', mousemoveHandler)
+        doc.addEventListener(evs.move, moveHandler)
 
-        function mousemoveHandler(ev) {
+        function moveHandler(ev) {
             let sl = de.scrollLeft || bd.scrollLeft,
                 st = de.scrollTop || bd.scrollTop,
-                left = sl + ev.clientX - dX,
-                top = st + ev.clientY - dY
+                left = sl + (ev.clientX || ev.touches[0].clientX) - dX,
+                top = st + (ev.clientY || ev.touches[0].clientY) - dY
             // limit
             const min = Math.min,
                 max = Math.max
@@ -26,23 +48,17 @@ function drag(el, doc = document, de = document.documentElement, bd = document.b
             el.style.cssText += `left:${left}px;top:${top}px;`
         }
 
-        doc.addEventListener('mouseup', mouseupHandler)
+        doc.addEventListener(evs.end, endHandler)
 
-        function mouseupHandler(ev) {
-            doc.removeEventListener('mousemove', mousemoveHandler)
-            doc.removeEventListener('mouseup', mouseupHandler)
+        function endHandler(ev) {
+            doc.removeEventListener(evs.move, moveHandler)
+            doc.removeEventListener(evs.end, endHandler)
         }
     }
-
-    return {
-        enable() {
-            el.addEventListener('mousedown', mousedownHandler)
-        },
-        disable() {
-            el.removeEventListener('mousedown', mousedownHandler)
-        }
-    }
-
+    el.addEventListener(evs.start, this.startHandler)
+}
+Drag.prototype.disable = function () {
+    this.el.removeEventListener(this.evs.start, this.startHandler)
 }
 
 // window.drag = drag
